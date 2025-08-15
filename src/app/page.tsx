48d185db9aa5625c787d112c45498084a967d829
IanWizard2029 <ian.messer@wizards.com>
2025-08-14 21:44:20 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-'use client'
-import Dashboard from '@/components/Dashboard'
+'use client';
+import Dashboard from '@/components/Dashboard';
 
-export default function Page(){
-  return <Dashboard />
+export default function Page() {
+  return <Dashboard />;
 }

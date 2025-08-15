785936f0f424b403ab6c2a1b479183807db9950a
IanWizard2029 <ian.messer@wizards.com>
2025-08-14 21:12:30 -0700

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,4 +1,15 @@
 import * as React from 'react';
-export function Checkbox({ checked, onChange, className='' }: { checked?: boolean, onChange?: () => void, className?: string }){
-  return <input type="checkbox" checked={!!checked} onChange={onChange} className={`h-4 w-4 rounded border-zinc-600 ${className}`} />
+export function Checkbox({
+  checked,
+  onChange,
+  className = ''
+}: { checked?: boolean; onChange?: () => void; className?: string }) {
+  return (
+    <input
+      type="checkbox"
+      checked={!!checked}
+      onChange={onChange}
+      className={`h-4 w-4 rounded border-zinc-600 bg-zinc-950/60 focus:ring-zinc-600 ${className}`}
+    />
+  );
 }

c8813c4ea04f74246f313e5fd333677efeb32788
IanWizard2029 <ian.messer@wizards.com>
2025-08-14 21:12:11 -0700

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,4 +1,13 @@
 import * as React from 'react';
-export function Input({ className='', ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={`px-3 py-2 rounded-lg bg-zinc-900 border border-zinc-800 text-zinc-100 placeholder:text-zinc-500 ${className}`} {...props} />
+
+export function Input({
+  className = '',
+  ...props
+}: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className={`h-9 px-3 rounded-lg bg-zinc-950/60 border border-zinc-800 text-zinc-100 placeholder:text-zinc-500 focus:outline-none focus:ring-1 focus:ring-zinc-600 ${className}`}
+      {...props}
+    />
+  );
 }
